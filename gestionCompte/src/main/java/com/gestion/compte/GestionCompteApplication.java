b790c93fe7d8aeab12a03afcf3421c2b3c3a6130
idabo <daboibrahima11@groupechaka.com>
2017-10-29 15:33:42 +0000

    ajout couche metier client

--- gestionCompte/src/main/java/com/gestion/compte/GestionCompteApplication.java
+++ gestionCompte/src/main/java/com/gestion/compte/GestionCompteApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.gestion.compte.entities.Client;
+import com.gestion.compte.metier.ClientMetier;
+
 @SpringBootApplication
 public class GestionCompteApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx= SpringApplication.run(GestionCompteApplication.class, args);
+/*		ClientMetier clientMetier=ctx.getBean(ClientMetier.class);
+		clientMetier.addClient(new Client("dabo"));
+		clientMetier.addClient(new Client("sylla"));
+		clientMetier.addClient(new Client("ba"));*/
 	}
 }
--- gestionCompte/src/main/java/com/gestion/compte/dao/ClientRepository.java
+++ gestionCompte/src/main/java/com/gestion/compte/dao/ClientRepository.java
@@ -1,9 +1,15 @@
 package com.gestion.compte.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gestion.compte.entities.Client;
 
 public interface ClientRepository extends JpaRepository<Client,Long>{
+	@Query("select c from client c where c.nom like :x")
+	public List<Client> clientsParMC(@Param("x") String mc);
 
 }
