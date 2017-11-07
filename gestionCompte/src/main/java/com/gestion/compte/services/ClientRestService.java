9d9f3dba4c0a69e4d425ebbe5168646e5ed0d1fa
idabo <daboibrahima11@groupechaka.com>
2017-11-07 11:04:24 +0000

    permission d'acces au microservice client

--- gestionCompte/src/main/java/com/gestion/compte/services/ClientRestService.java
+++ gestionCompte/src/main/java/com/gestion/compte/services/ClientRestService.java
@@ -3,6 +3,7 @@ package com.gestion.compte.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,10 +16,12 @@ import com.gestion.compte.metier.ClientMetier;
 public class ClientRestService {
 	@Autowired
 	private ClientMetier clientMetier;
+	
     @RequestMapping(value="/clients",method=RequestMethod.POST)
 	public Client addClient(@RequestBody Client c) {
 		return clientMetier.addClient(c);
 	}
+    @CrossOrigin
     @RequestMapping(value="/clients",method=RequestMethod.GET)
 	public List<Client> listClient() {
 		return clientMetier.listClient();
